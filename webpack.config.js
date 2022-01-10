c2ed815d22216f1160931b1d1ef9af3f321df32f
clayton <6334+clayton@users.noreply.github.com>
2022-01-09 17:35:50 -0700

    removes unused dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 
@@ -25,13 +24,7 @@ module.exports = {
     })
   ],
   optimization: {
-    minimizer: [
-      new UglifyJsPlugin({
-        cache: true,
-        parallel: true,
-        sourceMap: true, // set to true if you want JS source maps
-      }),
-    ],
+    
   },
   module: {
     rules: [
